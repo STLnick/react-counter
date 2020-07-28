0b5751fdeaa4a1f3dbec073dbb074bf58ed59145
Nick Ray <nickrray@gmail.com>
2020-07-28 15:33:07 -0500

    :sparkles: Finish dynamic update method using button dataset attributes

--- src/components/Display/Button/index.js
+++ src/components/Display/Button/index.js
@@ -4,13 +4,20 @@ import React from 'react'
 import './Button.css'
 
 export const Button = (props) => {
-  const handleClick = () => {
-    props.buttonHandler()
+  const handleClick = (e, qty) => {
+    props.buttonHandler(e, qty)
   }
 
   return (
-    <button onClick={handleClick}>{props.buttonText}</button>
+    <button onClick={(e) => handleClick(e, props.qty)} data-op={props.op} data-qty={props.qty}>
+      {props.buttonText}
+    </button>
   )
 }
 
-Button.propTypes = { buttonHandler: PropTypes.func, buttonText: PropTypes.string.isRequired }
+Button.propTypes = {
+  buttonHandler: PropTypes.func,
+  buttonText: PropTypes.string.isRequired,
+  op: PropTypes.string.isRequired,
+  qty: PropTypes.string.isRequired
+}
--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -3,14 +3,46 @@ import { Button } from './Button'
 
 import './Display.css'
 
+const styles = getComputedStyle(document.documentElement)
+
 export class Display extends React.Component {
   state = {
     currentCount: 0
   }
 
-  updateCount = () => {
-    this.setState(prevState => ({
-      currentCount: prevState.currentCount + 1
+  updateCount = (e, qty) => {
+    // Operator
+    const op = e.target.dataset.op
+
+    if (op === '+') { // Add
+      this.setState(prevState => ({
+        currentCount: prevState.currentCount + Number(qty)
+      }))
+    } else if (op === '-') { // Subtract
+      if (this.state.currentCount > 0) {
+        if (this.state.currentCount - Number(qty) >= 0) {
+          this.setState(prevState => ({
+            currentCount: prevState.currentCount - Number(qty)
+          }))
+        } else { // If subtraction will result in negative just set to 0
+          this.setState(prevState => ({
+            currentCount: 0
+          }))
+        }
+      }
+    } else { // Reset
+      this.setState(prevState => ({
+        currentCount: 0
+      }))
+    }
+    // const newFontSize = Number(styles.getPropertyValue('--p-font-size').slice(0, 3)) + 2
+    // console.log(newFontSize)
+    // styles.setProperty('--p-font-size', newFontSize + "px")
+  }
+
+  resetCount = () => {
+    this.setState(() => ({
+      currentCount: 0
     }))
   }
 
@@ -18,7 +50,12 @@ export class Display extends React.Component {
     return (
       <div>
         <p>{this.state.currentCount}</p>
-        <Button buttonHandler={this.updateCount} buttonText="Add 1" />
+        <Button buttonHandler={this.updateCount} buttonText="Add 1" op="+" qty="1" />
+        <Button buttonHandler={this.updateCount} buttonText="Add 2" op="+" qty="2" />
+        <Button buttonHandler={this.updateCount} buttonText="Add 5" op="+" qty="5" />
+        <Button buttonHandler={this.updateCount} buttonText="Subtract 1" op="-" qty="1" />
+        <Button buttonHandler={this.updateCount} buttonText="Subtract 10" op="-" qty="10" />
+        <Button buttonHandler={this.updateCount} buttonText="Reset" op="" qty="0" />
       </div>
     )
   }
